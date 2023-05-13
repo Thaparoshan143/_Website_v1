b5540e5d4c6e58639877067b20210fe86d12bdbf
Roshan Thapa <roshan@Roshans-MacBook-Air.local>
2023-05-13 15:51:21 +0545

    projects source link added and minor changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,15 +2,16 @@ import React from 'react'
 import "./Projects.css"
 import "../uni.css"
 
-const projectsTitle=["Web Programs","Software/App","Programs","Utility Programs"];
-const projectsSubTitle=[["Web Utility","SXJ Intra Quiz Program","SXJ Quiz Program"],["Review System","SXJ Inter Quiz Program"],["Student DBMS"],["Stock MP Utility","React Utility","RLibrary"]];
-const projectsSubTitleDescription=[["Web Based Program created from React JS desgined to serve the speed working with desgines and code generation","Web Based Program desgined to serve the need of Quiz Contest inside SXJ Premises","Web Based Program desgined to for organzining High school Quiz Contest inside SXJ"],
-                                    ["Application made inside Unity to serve the need of review system program in COAS (Exhibition)","Application made with Unity designed to serve the need of the Inter-School Quiz Contest oraganized by SXJ"],
-                                    ["Program written in C, with objective to make the organize the students data inside dbms (special showcase for Exhibition)"],
-                                    ["Program wriiten in Python, focused to get current Stock Market Price of given script and tolerance given for any task","Program wriiten in Python, desgined to serve the new setup and easy component adding feature for React JS with simple oneclick run","Library written in C++, which comes with bunch of useful functions and classes to speed up the fresh projects"]];
+const projectsTitle=["Web Programs","Software/App","Programs","Utility Programs","3D Modeling/Game Development"];
+const projectsSubTitle=[["Web Utility","Intra School Quiz SXJ","SXJ MCQS Quiz"],["Review and Voting System","Joint School Quiz SXJ"],["Mini Projects with C"],["Stock MP Utility","React Utility","RLibrary"],["3D Models and Games"]];
+const projectsSubTitleDescription=[["Web Based Program created from React JS desgined to serve the speed working with desgines and code generation","Web Based Program desgined to serve the need of Quiz Contest inside SXJ Premises","Web Based Program desgined to for organzining High school MCQS Contest within classes"],
+                                    ["Application made inside Unity to serve the need of review system and voting system program in COAS (Science Exhibition)","Application made with Unity designed to serve the need of the Inter-School Quiz Contest oraganized by SXJ"],
+                                    ["Written in C, containing different varities of programs to serve specific need and also can be added to any project as per required"],
+                                    ["Program wriiten in Python, focused to get current Stock Market Price of given script and tolerance given for any task","Program wriiten in Python, desgined to serve the new setup and easy component adding feature for React JS with simple oneclick run","Library written in C++, which comes with bunch of useful functions and classes to speed up the fresh projects"],
+                                    ["Projects done on Blender and Unity for learning and getting hands on with different way of making Models, render images, animation and along with Games made in Unity Game Engine"]];
 
-const toolsUsed=[[["React JS","CSS","JS"],["HTML","CSS","JS"],["HTML","CSS","JS"]],[["Unity","C#"],["Unity","C#"]],[["C"]],[["Python"],["Python"],["C++"]]];
-const viewSourceLinks=[["www.google.com"],[],[],[]];
+const toolsUsed=[[["React JS","CSS","JS"],["HTML","CSS","JS"],["HTML","CSS","JS"]],[["Unity","C#"],["Unity","C#"]],[["C"]],[["Python"],["Python"],["C++"]],[["Unity","Blender"]]];
+const viewSourceLinks=[["https://github.com/Thaparoshan143/WebUtility_react","https://drive.google.com/drive/folders/1kt8hL7HuYp5Zvi1MkAiAaoJJewIbu-Ph","https://drive.google.com/drive/folders/1QknB85wG_DAC38vcQnPdnFNu2K-Blg-T"],["https://drive.google.com/drive/folders/1CogkgVoLMab7Rn8X7FAmRhpQrcPxnMry","https://drive.google.com/drive/folders/1i78RFwqSctB2Jqs3WTGGWpF8SJ87eW2z"],["https://intriguing-steam-75f.notion.site/Mini-Projects-with-C-57801b3a17204c6c877660808901c275"],["https://github.com/Thaparoshan143/Stock_WebScr_python/tree/main","https://github.com/Thaparoshan143/webBoiGen_Python","https://github.com/Thaparoshan143/Rlibrary"],["https://www.youtube.com/@roshanthapa1297/videos"]];
 
 function Projects() {
   return (
