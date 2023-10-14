b760001d847cecc5732c033eb1ab5eb28b93b21b
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-10-14 09:21:59 +0545

    Adding the Info in file

---
+++ src/Info.js
@@ -0,0 +1,213 @@
+// This is the main data containing block
+
+
+export const Name="Roshan Thapa";
+export const Quote="\" An Energetic & Eager Computer Engineering Student \"";
+export const MyDescription = "I am Computer Engineering Student at Thapathali Campus. To describe in short, I am into Programming (C, C++, C#, Python), Web Developemt (HTML, CSS, JS, Reactjs), 3D Modeling (Blender). As a hobby, I also get involved in Game Development (Unity | C#). I am always looking forward to solve new challenges, learning and exploring new things in the technological world solving the problems using the tools known";
+
+export const MainNavItemsTitle = [
+    {
+        title : "Home",
+        url : "/"
+    },
+    {
+        title : "Skills",
+        url : "/"
+    },
+    {
+        title : "Projects",
+        url : "/"
+    },
+    {
+        title : "Know More",
+        url : "/"
+    },
+    {
+        title : "Get In Touch",
+        url : "/"
+    }
+];
+
+export const SocialHandles = [
+    {
+        media : "Facebook",
+        url : "https://www.facebook.com/Me-in-Unity-102966158048488/"
+    },
+    {
+        media : "Github",
+        url : "https://github.com/Thaparoshan143"
+    },
+    {
+        media : "Youtube",
+        url : "https://www.youtube.com/channel/UCgcp2_YxezJh71hxvcb_CWw"
+    },
+    {
+        media : "Linkedin",
+        url : "https://www.linkedin.com/in/roshan-thapa-294498251/"
+    }
+];
+
+export const MajorSkills = [
+    {
+        field : "Programming",
+        subItem : [
+            {
+                item : "C",
+                level : "Competent"
+            },
+            {
+                item : "C++",
+                level : "Competent"
+            },
+            {
+                item : "Python",
+                level : "Advanced Beginner"
+            },
+            {
+                item : "C#",
+                level : "Advanced Beginner"
+            }
+        ]
+    },
+    {
+        field : "Web Development",
+        subItem : [
+            {
+                item : "HTML",
+                level : "Competent"
+            },
+            {
+                item : "CSS",
+                level : "Competent"
+            },
+            {
+                item : "JS",
+                level : "Advanced Beginner"
+            },
+            {
+                item : "Reactjs",
+                level : "Advanced Beginner"
+            }
+        ]
+    },
+    {
+        field : "3D Artist",
+        subItem : [
+            {
+                item : "Blender",
+                level : "Advanced Beginner"
+            },
+            {
+                item : "Autodesk",
+                level : "Beginner"
+            }
+        ]
+    },
+    {
+        field : "Game Development",
+        subItem : [
+            {
+                item : "Unity",
+                level : "Advanced Beginner"
+            }
+        ]
+    },
+    {
+        field : "Others",
+        subItem : [
+            {
+                item : "Photoshop",
+                level : "Beginner"
+            }
+        ]
+    },
+];
+
+export const ProjectsInfo = [
+    {
+        type : "Web Programs",
+        subItem : [
+            {
+                item : "Web Utility",
+                description : "Web Based Program created from React JS desgined to serve the speed working with desgines and code generation",
+                tools : ["React JS","HTML","CSS","JS"],
+                url : "https://github.com/Thaparoshan143/WebUtility_react"
+            },
+            {
+                item : "Intra School Quiz SXJ",
+                description : "Web Based Program desgined to serve the need of Quiz Contest inside SXJ Premises",
+                tools : ["HTML","CSS","JS"],
+                url : "https://drive.google.com/drive/folders/1kt8hL7HuYp5Zvi1MkAiAaoJJewIbu-Ph"
+            },
+            {
+                item : "SXJ MCQS Quiz",
+                description : "Web Based Program desgined to for organzining High school MCQS Contest within classes",
+                tools : ["HTML","CSS","JS"],
+                url : "https://drive.google.com/drive/folders/1QknB85wG_DAC38vcQnPdnFNu2K-Blg-T"
+            }
+        ]
+    },
+    {
+        type : "Software/App",
+        subItem : [
+            {
+                item : "Review and Voting System",
+                description : "Application made inside Unity to serve the need of review system and voting system program in COAS (Science Exhibition)",
+                tools : ["Unity", "C#"],
+                url : "https://drive.google.com/drive/folders/1CogkgVoLMab7Rn8X7FAmRhpQrcPxnMry"
+            },
+            {
+                item : "Joint School SXJ Quiz",
+                description : "Application made with Unity designed to serve the need of the Inter-School Quiz Contest oraganized by SXJ",
+                tools : ["Unity", "C#"],
+                url : "https://drive.google.com/drive/folders/1i78RFwqSctB2Jqs3WTGGWpF8SJ87eW2z"
+            }
+        ]
+    },
+    {
+        type : "Programs",
+        subItem : [
+            {
+                item : "Mini Projects with C",
+                description : "Written in C, containing different varities of programs to serve specific need and also can be added to any project as per required",
+                tools : ["C"],
+                url : "https://intriguing-steam-75f.notion.site/Mini-Projects-with-C-57801b3a17204c6c877660808901c275"
+            }
+        ]
+    },
+    {
+        type : "Utility Programs",
+        subItem : [
+            {
+                item : "Stock Market Price Parser",
+                description : "Program wriiten in Python, with beautiful soup plugin focused to get current Stock Market Price of given script and tolerance given for any task",
+                tools : ["Python", "Beautiful Soup"],
+                url : "https://github.com/Thaparoshan143/Stock_WebScr_python"
+            },
+            {
+                item : "React Starter",
+                description : "Program wriiten in Python, desgined to serve the new setup and easy component adding feature for React JS with simple oneclick run",
+                tools : ["Python"],
+                url : "https://github.com/Thaparoshan143/webBoiGen_Python"
+            },
+            {
+                item : "RLibrary",
+                description : "Library written in C++, which comes with bunch of useful functions and classes to speed up the fresh projects",
+                tools : ["C++"],
+                url : "https://github.com/Thaparoshan143/Rlibrary"
+            }
+        ]
+    },
+    {
+        type : "3D Modeling/Game Development",
+        subItem : [
+            {
+                item : "3D Models and Games",
+                description : "Projects done on Blender and Unity for learning and getting hands on with different way of making Models, render images, animation and along with Games made in Unity Game Engine",
+                tools : ["Unity", "Blender"],
+                url : "https://www.youtube.com/@roshanthapa1297/videos"
+            }
+        ]
+    }
+];
+
